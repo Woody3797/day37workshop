35b34a5ca1b85eeb3b496c7e407e168465ee266a
Woody3797 <woody3797@gmail.com>
2023-05-23 23:23:06 +0800

    day37workshop

--- src/main/java/ibf2022/csf/day37workshop/controller/ImageController.java
+++ src/main/java/ibf2022/csf/day37workshop/controller/ImageController.java
@@ -1,30 +1,29 @@
-package ibf2022.csf.day37workshop.controller;
+// package ibf2022.csf.day37workshop.controller;
 
-import java.util.Optional;
+// import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.RestController;
 
-import ibf2022.csf.day37workshop.service.PostsService;
+// import ibf2022.csf.day37workshop.service.PostsService;
 
-@RestController
-public class ImageController {
+// @RestController
+// public class ImageController {
 
-    // @Autowired
-    // PostsService postsService;
+//     @Autowired
+//     PostsService postsService;
     
-    // @GetMapping(path = "/uploaded/{post_id}")
-    // public ResponseEntity<byte[]> getImage(@PathVariable String post_id) {
-    //     Optional<byte[]> image = postsService.getPicture(post_id);
+//     @GetMapping(path = "/uploaded/{post_id}")
+//     public ResponseEntity<byte[]> getImage(@PathVariable String post_id) {
+//         Optional<byte[]> image = postsService.getPicture(post_id);
 
-    //     if (image.isEmpty()) {
-    //         return ResponseEntity.notFound().build();
-    //     } else {
-    //         return ResponseEntity.ok().header("Content-Type", "image/*").body(image.get());
-    //     }
-    // }
-}
+//         if (image.isEmpty()) {
+//             return ResponseEntity.notFound().build();
+//         } else {
+//             return ResponseEntity.ok().header("Content-Type", "image/*").body(image.get());
+//         }
+//     }
+// }
